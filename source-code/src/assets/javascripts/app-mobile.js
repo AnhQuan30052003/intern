8bb15be5db95c8288ebcd1cb06de4fbd46f2107c
Admin <anhquan300503@gmail.com>
2023-06-07 13:12:52 +0700

    update file 07/06/2023

--- source-code/src/assets/javascripts/app-mobile.js
+++ source-code/src/assets/javascripts/app-mobile.js
@@ -16,6 +16,7 @@ import 'modules/FormAnimation'
 import 'modules/UserAgent'
 import 'modules/Header'
 import 'modules/Menu'
+import 'modules/NavBar'
 import 'modules/AnimationScrollPage'
 import 'modules/ConvertSvg'
 
