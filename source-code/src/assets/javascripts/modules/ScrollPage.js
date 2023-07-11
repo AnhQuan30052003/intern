c362cd0deb8a4be024c99f920d2196e7f8e94bcd
AnhQuan30052003 <anhquan300503@gmail.com>
2023-07-11 09:55:16 +0700

    11/07/2022: update scroll page

---
+++ source-code/src/assets/javascripts/modules/ScrollPage.js
@@ -0,0 +1,29 @@
+export default class ScrollPage {
+  constructor () {
+    this.$this = $('body');
+  }
+  init () {
+    if (this.$this.length) {
+      this.loadAnimation();
+    }
+  }
+  loadAnimation() {
+    window.addEventListener('scroll', reveal);
+    function reveal(){
+      var reveals = document.querySelectorAll('.reveal');
+      for(var i = 0 ; i < reveals.length ; i++) {
+        var windowheight = window.innerHeight;
+        var revealtop = reveals[i].getBoundingClientRect().top;
+        var revealpoint = 150;
+
+        if(revealtop < windowheight - revealpoint){
+          reveals[i].classList.add('active');
+        }
+        else{
+          reveals[i].classList.remove('active');
+        }
+      }
+    }
+  }
+}
+new ScrollPage().init()
