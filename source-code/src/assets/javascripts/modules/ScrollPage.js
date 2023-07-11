1a0e3dbce2e19672b8904fdd13ab8bb1e6c92919
AnhQuan30052003 <anhquan300503@gmail.com>
2023-07-11 12:59:36 +0700

    10/07/2023: the last update source cty

--- source-code/src/assets/javascripts/modules/ScrollPage.js
+++ source-code/src/assets/javascripts/modules/ScrollPage.js
@@ -9,17 +9,17 @@ export default class ScrollPage {
   }
   loadAnimation() {
     window.addEventListener('scroll', reveal);
-    function reveal(){
+    function reveal() {
       var reveals = document.querySelectorAll('.reveal');
-      for(var i = 0 ; i < reveals.length ; i++) {
+      for (var i = 0 ; i < reveals.length ; i++) {
         var windowheight = window.innerHeight;
         var revealtop = reveals[i].getBoundingClientRect().top;
-        var revealpoint = 150;
+        var revealpoint = 50;
 
-        if(revealtop < windowheight - revealpoint){
+        if (revealtop < windowheight - revealpoint) {
           reveals[i].classList.add('active');
         }
-        else{
+        else {
           reveals[i].classList.remove('active');
         }
       }
